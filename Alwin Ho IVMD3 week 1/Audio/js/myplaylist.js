fe66f4cde71aa0845b50c4ea3c98d529871c706d
Hokaki <alwin-kakit@hotmail.com>
2015-02-09 13:02:17 +0100

    week1

---
+++ Alwin Ho IVMD3 week 1/Audio/js/myplaylist.js
@@ -0,0 +1,44 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/Jakubi - Cannot Afford It All.mp3',
+        oga:'mix/Jakubi - Cannot Afford It All.ogg',
+        title:'Jakubi - Cannot Afford It All',
+        artist:'jakubi',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:43',
+        cover:'mix/jakubi.png'
+    },
+    {
+        mp3:'mix/Jakubi - Couch Potato.mp3',
+        oga:'mix/Jakubi - Couch Potato.ogg',
+        title:'Jakubi - Couch Potato',
+        artist:'jakubi',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:15',
+        cover:'mix/jakubi.png'
+    },
+        {
+        mp3:'mix/Jakubi - Holiday.mp3',
+        oga:'mix/Jakubi - Holiday.ogg',
+        title:'Jakubi - Holiday',
+        artist:'jakubi',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:43',
+        cover:'mix/jakubi.png'
+    }
+];
